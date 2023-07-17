044ff09515865e86321070019cf939e7ec789222
ethanhuang <ethanhuang0606@gmail.com>
2023-07-16 17:48:06 -0700

    minimum config for next-mdx-remote to work in blog (server conponent)

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Content from '@components/Content'
 import Link from 'next/link'
 
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+
 export default function Home() {
   return (
     <Content title="writings">
       <div className="grid grid-row-3 gap-24">
         <div>
           <div className="text-1xl">Sample Blog Content</div>
+          <MDXRemote source={`# Hello World This is from Server Components!`}/>
         </div>
       </div>
     </Content>
