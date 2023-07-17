213139391b9e9b097227432f93ed004883447b1f
ethanhuang <ethanhuang0606@gmail.com>
2023-07-16 17:25:14 -0700

    known working, by removing contentlayer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const { withContentlayer } = require('next-contentlayer')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    mdxRs: true,
-  },
-  reactStrictMode: true,
-  swcMinify: true ,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig;
